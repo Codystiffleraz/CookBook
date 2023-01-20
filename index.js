d647539c8f7875b3bb9c73154af7471b23464a7e
Cody Stiffler <codystiffleraz@gmail.com>
2023-01-20 01:29:08 -0700

    Co-authored-by: juan-ruiz-dev <juan-ruiz-dev@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+app.use(express.static((__dirname, 'public')));
+
+
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Up on 3k")
+})
 module.exports = app
